dc5c4d7331e7670739859ebf522d89db280f8c2a
Raphael Menges <Raphael.Menges@alfatraining.de>
2025-08-18 11:01:52 +0200

    Disable `rust-cxx` for now.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "rust_cxx/lib.h"
+// #include "rust_cxx/lib.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
@@ -16,8 +16,8 @@ int main(int argc, char* argv[])
 	engine.rootContext()->setContextProperty(
 	    "helloFromRustCCrate",
 	    QString::fromStdString(std::string("Number from Rust-C-crate: " + std::to_string(get_forty_two()))));
-	engine.rootContext()->setContextProperty("helloFromRustCxxCrate",
-	                                         QString::fromStdString(std::string(rust_cxx::hello_world())));
+	// engine.rootContext()->setContextProperty("helloFromRustCxxCrate",
+	//                                          QString::fromStdString(std::string(rust_cxx::hello_world())));
 	engine.load(QUrl("qrc:///main.qml"));
 	return app.exec();
 }
--- rust-c/src/lib.rs
+++ rust-c/src/lib.rs
@@ -4,4 +4,5 @@ use nalgebra::Vector3;
 pub extern "C" fn get_forty_two() -> i32 {
     let foo = Vector3::from_row_slice(&[10, 20, 12]);
     foo.x + foo.y + foo.z
+    // 42
 }
